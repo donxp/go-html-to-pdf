2c02729dd393ed84e194436f5ddf6d1ffb5aa78a
donxp <don.paplauskas@cushon.co.uk>
2023-07-11 23:14:43 +0100

    Implement http server for generating documents

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	// Start chrome context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
 	// Register HTTP handler
+	http.HandleFunc("/generate", handlePostGenerate)
 
 	// Start HTTP server
+	fmt.Println("Starting HTTP server")
+	http.ListenAndServe(fmt.Sprintf(":%d", mustGetHttpServerPort()), nil)
+}
 
-	html := "<html><body><h1>Hello World</h1></body></html>"
+func htmlToPdf(html string) []byte {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
 
 	var buf []byte
 	if err := chromedp.Run(ctx,
@@ -31,10 +37,34 @@ func main() {
 			return page.SetDocumentContent(frameTree.Frame.ID, html).Do(ctx)
 		}),
 		printToPDF(&buf)); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	os.WriteFile("sample.pdf", buf, 0o644)
+	return buf
+}
+
+func handlePostGenerate(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		bearerToken := strings.Replace(r.Header.Get("Authorization"), "Bearer ", "", 1)
+
+		if mustGetApiToken() != bearerToken {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		body, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			panic(err)
+		}
+		
+		htmlBody := string(body[:])
+		generatedPdf := htmlToPdf(htmlBody)
+		w.Write(generatedPdf)
+	default:
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
 }
 
 func printToPDF(res *[]byte) chromedp.ActionFunc {
@@ -47,4 +77,19 @@ func printToPDF(res *[]byte) chromedp.ActionFunc {
 		*res = buf
 		return nil
 	})
+}
+
+func mustGetHttpServerPort() int {
+	var serverPort int64 = 3000
+	port, success := os.LookupEnv("HTML_TO_PDF_PORT")
+	if success {
+		serverPort, _ = strconv.ParseInt(port, 10, 32)
+	}
+
+	return int(serverPort)
+}
+
+func mustGetApiToken() string {
+	token := os.Getenv("API_TOKEN")
+	return token
 }
